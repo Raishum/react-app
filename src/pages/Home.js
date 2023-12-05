375f53f36ea2f975d35100681570ddf7827224b3
Raishum <raishum.singh.006@accesstomusic.ac.uk>
2023-12-05 13:07:13 +0000

    Updated code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,8 @@ const Home = ()=> {
       <div>
         <h1>GibJohn Tutoring</h1>
         <h2>Unlock Your Potential with GibJohn Tutoring!<p>Elevate your learning experience with personalized guidance tailored just for you.</p></h2>
-        
       </div>
+
     );
   }
 export default Home;
--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import "./Pricing.css"
+import asda from "../price.png"
 
   function Pricing(){
     return(
       <div>
         <h1>Pricing</h1>
-        <h2>This tutoring is amined to help chilrdrn aged 11-16 who are strugling in school or wnat to achive the best grade possible in their GCSES</h2>
+        <h2>This tutoring program is designed to support students aged <p> 11-16 who may be facing challenges in school or are striving to achieve the highest grades possible in their GCSEs.</p></h2>
+        <img class = "asda" id="price" src= {asda} alt="The Pulpit Rock" width="304" height="228"></img>
         <h3><ul>
           <li>Cost for 3 months - £350</li>
           <li>Cost for 6 months - £600</li>
           <li>Cost for 1 year - £1500</li>
           </ul></h3>
       </div>
+
+    
     );
   }
   export default Pricing;
